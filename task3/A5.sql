1d8f7f2922d787f635e5e5cd217910c9573a63b0
Michael Ion <michael.antonius.ion@gmail.com>
2016-12-29 10:46:44 +0100

    format A5

--- task3/A5.sql
+++ task3/A5.sql
@@ -1,4 +1,4 @@
-select c.Name, sum(s.OrderLineProfit)
+select c.Name as 'Customer Name', sum(s.OrderLineProfit) as 'Profit'
 from DM_FactSales s, DM_Customer c
 where s.CustomerID = c.CustomerID and s.OrderDate >= '2014-01-01' and  s.OrderDate < '2014-07-01' 
 group by c.Name
